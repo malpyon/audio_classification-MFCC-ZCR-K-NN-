e3aea90b021f30e7bc6be2c2cf3ff98d3f2b5041
malpyon <50171515+malpyon@users.noreply.github.com>
2023-12-12 12:35:53 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 
 st.write("""
 # MFCC & ZCR audio classification with K-NN
@@ -20,3 +23,14 @@ scaler = pickle.load(open('scaler.pkl', 'rb'))
 x_scaled = scaler.transform(x)
 df_x_scaled = pd.DataFrame(data=x_scaled, columns=['MFCC'+str(x) for x in range(1,21)])
 st.write(df_x_scaled)
+
+st.subheader('accuracy with k-nn')
+
+x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size = 0.20, random_state = 0)
+model = pickle.load(open('model.pkl', 'rb'))
+y_pred = model.predict(x_test)
+cv_scores = cross_val_score(model, x_train, y_train, cv=10)
+accuracy_score = pd.DataFrame(data=[cv_scores.mean()], columns=['accuracy'])
+
+st.write(accuracy_score)
+
